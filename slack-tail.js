44031053e8daa9d2c27c8d20bc01961f865c8d73
kumauta <nogami.s@gmail.com>
2018-06-02 20:42:34 +0900

    format

--- slack-tail.js
+++ slack-tail.js
@@ -13,7 +13,7 @@ var rtm = new RtmClient(token, {
 
 rtm.start();
 
-rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
   var user = rtm.dataStore.getUserById(rtm.activeUserId);
   var team = rtm.dataStore.getTeamById(rtm.activeTeamId);
   console.log('Connected to ' + team.name + ' as ' + user.name);
@@ -23,29 +23,29 @@ rtm.on('message', (event) => {
   //var channel = rtm.dataStore.getGroupById(event.team);
   var channel = rtm.dataStore.getChannelById(event.channel) || rtm.dataStore.getGroupById(event.channel) || rtm.dataStore.getDMById(event.channel);
   var channelName = "";
-  if ( channel.name) {
+  if (channel.name) {
     channelName = "#" + channel.name;
-  }else{
+  } else {
     channelName = "@" + rtm.dataStore.getUserById(channel.user).name;
   }
-  channelName = (channelName + "              ").slice(0,17);
+  channelName = (channelName + "              ").slice(0, 17);
 
   var userName = 'system';
-  if ( rtm.dataStore.getUserById(event.user) ){
+  if (rtm.dataStore.getUserById(event.user)) {
     userName = rtm.dataStore.getUserById(event.user).name;
   }
-  userName = ("@" + userName + "              ").slice(0,17);
+  userName = ("@" + userName + "              ").slice(0, 17);
 
   var message = 'system operation';
-  if ( event.text ) {
+  if (event.text) {
     message = event.text;
   }
 
   var date = new Date(Math.floor(event.ts * 1000));
-  var month = ("0"+(date.getMonth() + 1)).slice(-2);
-  var day = ("0"+date.getDate()).slice(-2);
-  var hour = ("0"+date.getHours()).slice(-2);
-  var minutes = ("0"+date.getMinutes()).slice(-2);
+  var month = ("0" + (date.getMonth() + 1)).slice(-2);
+  var day = ("0" + date.getDate()).slice(-2);
+  var hour = ("0" + date.getHours()).slice(-2);
+  var minutes = ("0" + date.getMinutes()).slice(-2);
   var dispDate = month + '/' + day + ' ' + hour + ':' + minutes;
 
   console.log(dispDate + ' - ' + channelName + ' ' + userName + ': ' + message);
